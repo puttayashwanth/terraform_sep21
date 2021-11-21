a6f3b0edad0e321ee348fce6c70654c128ac7818
Harsha <jaintpahrsha@gmail.com>
2021-11-21 10:11:51 +0530

    Provisioners file

--- modules/remote-exec/main.tf
+++ modules/remote-exec/main.tf
@@ -12,10 +12,9 @@ resource "null_resource" "module_remote_exec" {
 				"sudo apt update -y", 
 				"sudo apt-get install -y ca-certificates curl gnupg lsb-release",
 				"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-				"echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+				"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
 				"sudo apt-get update -y",
 				"sudo apt-get -y install docker-ce docker-ce-cli containerd.io"	
 		]
 	}
-
 }
